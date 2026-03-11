009e7f5a9872e3ef43fcb1c66fe4b2a2860effb4
ruaaabumqadam <ruaaabumqa@example.com>
2026-03-11 07:35:08 +0200

    Improve welcome message in bikeshare script

--- bikeshare.py
+++ bikeshare.py
@@ -15,8 +15,8 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    print("Hello! Let's explore some US bikeshare data!")
+    print("You can filter the data by city, month, and day.")    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
 
 
     # get user input for month (all, january, february, ... , june)
